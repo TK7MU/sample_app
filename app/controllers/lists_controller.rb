eabcedfea21233ed343e3a71c852ca4d5e5f8072
EC2 Default User <ec2-user@ip-172-31-30-122.ap-northeast-1.compute.internal>
2023-04-16 08:37:40 +0000

    アプリケーションを作成しよう第2章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,24 @@
 class ListsController < ApplicationController
   def new
+  @list = List.new  
   end
 
-  def index
-  end
 
-  def show
-  end
+def create
 
-  def edit
-  end
+list=List.new(list_params)
+
+list.save
+
+redirect_to'/top'
 end
+
+
+private
+
+def list_params
+  params.require(:list).permit(:title,:body)
+end
+end
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  get '/top' => 'homes#top'
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-    get 'homes/top'
-  # For details oget on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'lists' => 'lists#create'   
 end
