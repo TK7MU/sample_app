bc7f2edd3ada89fb92ed90490b324bbba8213dbf
EC2 Default User <ec2-user@ip-172-31-30-122.ap-northeast-1.compute.internal>
2023-04-17 07:25:27 +0000

    アプリケーションを作成しよう第３章途中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,24 +1,12 @@
 class ListsController < ApplicationController
-  def new
-  @list = List.new  
+
+
+
+
+  def show
+    @list = List.find(params[:id])  
   end
 
 
-def create
 
-list=List.new(list_params)
-
-list.save
-
-redirect_to'/top'
 end
-
-
-private
-
-def list_params
-  params.require(:list).permit(:title,:body)
-end
-end
-
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
   get 'lists/new'
-  post 'lists' => 'lists#create'   
-end
+  get 'lists/edit'
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show'end
