f9d110d03ed865724ce4d77ac656014ce94c470b
EC2 Default User <ec2-user@ip-172-31-30-122.ap-northeast-1.compute.internal>
2023-04-19 13:49:17 +0000

    アプリケーションを作成しよう第９章終了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,9 +10,12 @@ def index
 end
   
 def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id) 
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
 end
   
 def show
